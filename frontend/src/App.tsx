d3ceed03d549ef59c138afd5cb85e20c5ebe71ea
Gergely Sabli <139555565+sabligeri@users.noreply.github.com>
2024-04-16 15:29:17 +0200

    note.tsx implemented and created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import MyTest from './components/MyTest'
+import Note from './components/Note'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <h1>Hello!</h1>
       <MyTest/>
+      <Note />
     </>
   )
 }
