47bf2ae8a7a1bbfadbb33ee019e3ce6419c850b2
Varga Bálint <65251273+Kekalma@users.noreply.github.com>
2024-04-16 12:52:32 +0200

    User basic operations finished

---
+++ backend/src/main/java/com/codecool/men/UserDAO.java
@@ -0,0 +1,5 @@
+package com.codecool.men;
+
+public interface UserDAO {
+
+}
--- backend/src/main/java/com/codecool/men/controller/UserController.java
+++ backend/src/main/java/com/codecool/men/controller/UserController.java
@@ -3,27 +3,37 @@ package com.codecool.men.controller;
 import com.codecool.men.controller.components.NewUser;
 import com.codecool.men.controller.components.Note;
 import com.codecool.men.controller.components.User;
+import com.codecool.men.dtos.UserOperationsDTO;
+import com.codecool.men.service.UserService;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+  private UserService userService;
 
-  @GetMapping("/login")
-  public boolean loginUser(@RequestBody NewUser newUser) {
-    throw new RuntimeException();
+  public UserController(UserService userService) {
+    this.userService = userService;
   }
 
-  @GetMapping("/edit")
-  public User editUser(@RequestBody User user) {
+  @GetMapping("/login")
+  public ResponseEntity<?> loginUser(@RequestBody UserOperationsDTO userOperationsDTO) {
+    return userService.loginUser(userOperationsDTO);
+  }
+
+  @GetMapping("/edit/{userId}")
+  public User editUser(@RequestParam UUID userID, @RequestBody User user) {
     throw new RuntimeException();
   }
 
   @GetMapping("/delete/{userId}")
-  public boolean delete(@RequestParam int userId) {
-    throw new RuntimeException();
+  public void delete(@RequestParam UUID userId) {
+    userService.deleteUser(userId);
   }
 
   @GetMapping("/add")
