68fb83a2b5d788390b51382c16a03474ff571323
korodigergo <127103460+korodigergo@users.noreply.github.com>
2024-04-16 11:56:35 +0200

    implemented endpoints for note

--- backend/src/main/java/com/codecool/men/dao/note/NoteDAO.java
+++ backend/src/main/java/com/codecool/men/dao/note/NoteDAO.java
@@ -1,9 +1,13 @@
 package com.codecool.men.dao.note;
 
+import com.codecool.men.controller.components.NewNote;
 import com.codecool.men.controller.components.Note;
 
 import java.util.List;
 
 public interface NoteDAO {
-    List<Note> getAllNotes();
+    List<Note> getAllNotes(int userId);
+    Note getNote(int userId, int noteId);
+    boolean deleteNote(int userId, int noteId);
+    Note addNote(int userId, NewNote newNote);
 }
