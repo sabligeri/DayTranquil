b5664a8ac64875c660872070c4ae348a60823259
korodigergo <127103460+korodigergo@users.noreply.github.com>
2024-04-18 09:20:39 +0200

    implemented login page and register on one page

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,45 +1,82 @@
-import { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+
+async function addUser(name: string, password: string) {
+  console.log(name);
+  console.log(password);
+  const userToAdd = {
+    name,
+    password,
+  };
+
+  
+
+  try {
+    const addUserResponse = await fetch("/api/user/add", {
+      method: "POST",
+      body: JSON.stringify(userToAdd),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const addedUser = await addUserResponse.json();
+
+    console.log("registered: " + addedUser);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default function Login() {
-  const [name, setUsername] = useState('');
-  const [password, setUserPassword] = useState('');
+  const [name, setUsername] = useState("");
+  const [password, setUserPassword] = useState("");
   const [userId, setUserId] = useState(-1);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    const id = JSON.parse(localStorage.getItem('userId')!);
+    const id = JSON.parse(localStorage.getItem("userId")!);
     if (id > 0) {
       setUserId(id);
     }
   }, []);
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await addUser(name, password);
+
+    setUsername("");
+    setUserPassword("");
+  };
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     const user = {
       name,
-      password
+      password,
     };
 
     try {
-      const response = await fetch('/api/user/login', {
-        method: 'POST',
+      const response = await fetch("/api/user/login", {
+        method: "POST",
         body: JSON.stringify(user),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       });
       console.log("1." + response);
-      
+
       if (response.ok) {
         const responseUser = await response.json();
-        localStorage.setItem('userId', JSON.stringify(responseUser.userID));
-        setUserId(responseUser.userID)
-        navigate('/main');
+        localStorage.setItem("userId", JSON.stringify(responseUser.userID));
+        setUserId(responseUser.userID);
+        navigate("/main");
       } else {
-        console.error('Failed to add the user');
+        console.error("Failed to add the user");
       }
     } catch (error) {
       console.error(error);
@@ -53,52 +90,119 @@ export default function Login() {
   }
 
   return userId > 0 ? (
-    <form className="form" onSubmit={(e) => handleLogout(e)}>
-      <h4>You are logged in! </h4>
-      <button type="submit" className="btn">
+    <form className="login-layout" onSubmit={(e) => handleLogout(e)}>
+      <h2>You are logged in! </h2>
+      <button type="submit" className="login-page-btn">
         Log out
       </button>
     </form>
   ) : (
-    <div className="root-layout">
-      <form className="form" onSubmit={(e) => handleSubmit(e)}>
-        <h4>Login: </h4>
-        <div className="form-row">
-          <label htmlFor="userName" className="form-label">
-            Username 
+    <div className="main">
+      <input type="checkbox" id="chk" aria-hidden="true" />
+      <div className="signup">
+        <form onSubmit={(e) => handleRegister(e)}>
+          <label htmlFor="chk" aria-hidden="true">
+            Sign up
           </label>
           <input
             type="text"
-            id="userName"
             name="userName"
-            className="form-input"
             onChange={(e) => setUsername(e.target.value)}
             value={name}
             placeholder="Username"
             required
           />
-        </div>
-        <div className="form-row">
-        <label htmlFor="userName" className="form-label">
-            Password
-          </label>
           <input
             type="password"
             onChange={(e) => setUserPassword(e.target.value)}
             value={password}
             placeholder="Password"
           />
-        </div>
-        <button type="submit" className="btn">
-          Login
-        </button>
-        <p>
-        Not a member?
-        <Link to="/register" className="member-btn">
-          Register
-        </Link>
-      </p>
-      </form>
+          <button type="submit" className="login-page-btn">
+            Sign up
+          </button>
+        </form>
+      </div>
+      <div className="login">
+        <form onSubmit={(e) => handleSubmit(e)}  >
+          <label htmlFor="chk" aria-hidden="true">
+            Login
+          </label>
+          <input
+            type="text"
+            name="userName"
+            onChange={(e) => setUsername(e.target.value)}
+            value={name}
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            onChange={(e) => setUserPassword(e.target.value)}
+            value={password}
+            placeholder="Password"
+          />
+          <button type="submit" className="login-page-btn">
+            Log in
+          </button>
+        </form>
+      </div>
     </div>
+
+    //   <div class="main">
+    // 	<input type="checkbox" id="chk" aria-hidden="true">
+
+    // 		<div class="signup">
+    // 			<form>
+    // 				<label for="chk" aria-hidden="true">Sign up</label>
+    // 				<input type="text" name="txt" placeholder="User name" required="">
+    // 				<input type="email" name="email" placeholder="Email" required="">
+    // 				<input type="password" name="pswd" placeholder="Password" required="">
+    // 				<button>Sign up</button>
+    // 			</form>
+    // 		</div>
+
+    // 		<div class="login">
+    // 			<form>
+    // 				<label for="chk" aria-hidden="true">Login</label>
+    // 				<input type="email" name="email" placeholder="Email" required="">
+    // 				<input type="password" name="pswd" placeholder="Password" required="">
+    // 				<button>Login</button>
+    // 			</form>
+    // 		</div>
+    // </div>
   );
+}
+
+{
+  /* <p>Not a member?</p>
+<p>
+  <Link to="/register" className="login-link-btn">
+    Sign up
+  </Link>
+</p> */
+}
+{
+  /* <div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true">
+
+			<div class="signup">
+				<form>
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input type="text" name="txt" placeholder="User name" required="">
+					<input type="email" name="email" placeholder="Email" required="">
+					<input type="password" name="pswd" placeholder="Password" required="">
+					<button>Sign up</button>
+				</form>
+			</div>
+
+			<div class="login">
+				<form>
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" required="">
+					<input type="password" name="pswd" placeholder="Password" required="">
+					<button>Login</button>
+				</form>
+			</div>
+	</div> */
 }
