23aa054c48d4c2fd97aa3d7903f1bc70a3f21ed1
Gergely Sabli <139555565+sabligeri@users.noreply.github.com>
2024-05-02 15:37:04 +0200

    adding favourite button and date input

--- frontend/src/components/note/NoteList.tsx
+++ frontend/src/components/note/NoteList.tsx
@@ -1,3 +1,6 @@
+import Note from "./Note";
+
+
 interface Note {
   created: Date;
   id: number;
@@ -11,17 +14,19 @@ interface NoteListProps {
   notes: Note[];
 }
 
-export default function NoteList({del, notes}:NoteListProps) {
+export default function NoteList({ del, notes }: NoteListProps) {
   return (
     <div id="note-list">
       {/* <h2>Your Notes</h2> */}
       <div id="note-grid">
         {notes && notes.map((note, index: number) => (
-          <div id="note" key={index}>
-            <h3>{note.title}</h3>
-            <h3 className="note-text">{note.text}</h3>
-            <button onClick={() => del(note.id)}>Delete</button>
-          </div>
+          <Note
+            title={note.title}
+            text={note.text}
+            index={index}
+            del={del}
+            noteId={note.id}
+          />
         ))}
       </div>
     </div>
