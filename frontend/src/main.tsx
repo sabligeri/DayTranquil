9012270db15af9929697a6dcf074127d4a94101f
korodigergo <127103460+korodigergo@users.noreply.github.com>
2024-05-13 11:17:52 +0200

    implemented new page shoppinglist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,36 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Login from './components/Login.js';
-import App from './App.js';
-import Settings from './components/Settings.js';
-import NotePage from './components/note/NotePage.js';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./components/Login.js";
+import App from "./App.js";
+import Settings from "./components/Settings.js";
+import NotePage from "./components/note/NotePage.js";
+import ShoppingList from "./components/note/ShoppingList.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login/>,
+    element: <Login />,
   },
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
     children: [
       {
         path: "/main",
-        element: <NotePage/>
+        element: <NotePage />,
+      },
+      {
+        path: "/shoppinglist",
+        element: <ShoppingList />,
       },
       {
         path: "/settings",
-        element: <Settings/>
-      }
-    ]
+        element: <Settings />,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('main') as HTMLDivElement).render(
+ReactDOM.createRoot(document.getElementById("main") as HTMLDivElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
